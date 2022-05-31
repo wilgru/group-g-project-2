156e6afb44cb874d1808d4e0da9d88a8dc10f711
ElsieMay <elsiemaylawrie1@gmail.com>
2022-05-31 19:28:18 +1000

    fixed attributes listed in get request

--- controllers/clientPageRoutes.js
+++ controllers/clientPageRoutes.js
@@ -6,14 +6,7 @@ const Client = require("../models/Client");
 router.get("/list", async (req, res) => {
 	try {
 		// Get all clients and JOIN with projects data
-		const clientData = await Post.findAll({
-			include: [
-				{
-					model: Client,
-					attributes: ["id", "firstName", "lastName", "email", "phone"],
-				},
-			],
-		});
+		const clientData = await Client.findAll({});
 
 		// Serialize data so the template can read it
 		const clients = clientData.map((posts) => posts.get({ plain: true }));
@@ -24,6 +17,7 @@ router.get("/list", async (req, res) => {
 			logged_in: req.session.logged_in,
 		});
 	} catch (err) {
+		console.error(err);
 		res.status(500).json(err);
 	}
 });
