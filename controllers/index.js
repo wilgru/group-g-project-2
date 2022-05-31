d4ae1e5a14330c1289da3d24946a9686fbcd49a5
wilgru <william.gruszka@gmail.com>
2022-05-31 18:51:44 +1000

    usinng routes for client and projects

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
+// const withAuth = require('../utils/auth');
 
 // Import our modular routers for /tips and /feedback
 const api = require('./api');
 const projectRoutes = require('./projectsRoutes');
+const clientRoutes = require('./clientRoutes');
 
 const app = express();
 
 app.use('/api', api);
-// make sure this is last
-app.use('/', projectRoutes);
+app.use('/client', clientRoutes);
+app.use('/project', projectRoutes);
+
+//very last cacth-all endpoint
+router.get('*', (req, res) => {
+    res.render('home');
+});
 
 module.exports = app;
