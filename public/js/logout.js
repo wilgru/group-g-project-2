0ae96d46338be20b87569a6a197ad28d90d0edcf
wilgru <william.gruszka@gmail.com>
2022-06-01 16:37:25 +1000

    added fetch requests to API

---
+++ public/js/logout.js
@@ -0,0 +1,17 @@
+const logoutButton = document.getElementById('logout');
+
+const logout = async () => {
+  const response = await fetch('/api/manager/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/login');
+  } else {
+    alert(response.statusText);
+  }
+};
+
+
+logoutButton.addEventListener('click', logout);
