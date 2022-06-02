2e63482fbbcc76930552febb224f492af0b3b418
wilgru <william.gruszka@gmail.com>
2022-06-02 21:13:27 +1000

    added support for query params

--- controllers/projectPageRoutes.js
+++ controllers/projectPageRoutes.js
@@ -1,21 +1,54 @@
 const router = require("express").Router();
 const withAuth = require('./../middlewares/withAuth');
 const { Project, Client, Manager } = require("../models");
+const { Op, Sequelize } = require("sequelize");
 
 // project routes
 router.get("/list", async (req, res) => {
 	try {
-		// Get all projects and JOIN with projects data
-		const projectData = await Project.findAll({});
+		let projectData;
+		let isSearch = false;
+		let searchMessage = "";
+		let searchTerm = "";
+
+		// if ther are query params
+		if (req.query.q) {
+			isSearch = true;
+			searchTerm = req.query.q;
+
+			console.log(searchTerm)
+			// Get one projects by their first name
+			projectData = await Project.findAll({
+				where: {
+					projectName: {
+						[Op.substring]: searchTerm
+					}
+				}
+			});
+
+			searchMessage = `Found ${projectData.length} results for '${searchTerm}'`
+
+		} else {
+			// Get all projects
+			projectData = await Project.findAll();
+		}
+
+		if (!projectData) {
+			res.redirect('/');
+			return;
+		}
 
 		// Serialize data so the template can read it
-		const projects = projectData.map((project) => project.get({ plain: true }));
+		const projects = projectData.map((posts) => posts.get({ plain: true }));
 
 		// Pass serialized data and session flag into template
 		res.render("projectList", {
+			isSearch,
+			searchMessage,
+			searchTerm,
 			projects,
 			logged_in: req.session.logged_in,
-			manager_name: req.session.manager_name,
+			manager_name: req.session.manager_name
 		});
 	} catch (err) {
 		console.error(err);
@@ -24,6 +57,7 @@ router.get("/list", async (req, res) => {
 });
 
 router.get("/add", (req, res) => {
+
 	res.render("projectAdd", {
 		logged_in: req.session.logged_in,
 		manager_name: req.session.manager_name,
