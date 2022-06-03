3bfe2ff09b3180fe61fe2b54d933ae7c341bb8e2
wilgru <william.gruszka@gmail.com>
2022-06-03 13:14:11 +1000

    padded hhours and minutes

--- public/js/home.js
+++ public/js/home.js
@@ -2,7 +2,7 @@ const dateTime = document.getElementById('time')
 
 const getTime = () => {
     const newTime = new Date()
-    dateTime.textContent = `${newTime.getDate()}/${newTime.getMonth() + 1}/${newTime.getFullYear()} ${newTime.getHours()}:${newTime.getMinutes()}`
+    dateTime.textContent = `${newTime.getDate()}/${newTime.getMonth() + 1}/${newTime.getFullYear()} ${String(newTime.getHours()).padStart(2, '0')}:${String(newTime.getMinutes()).padStart(2, '0')}`
 }
 
 getTime()
