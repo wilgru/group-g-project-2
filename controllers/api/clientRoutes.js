13bebc9fd1b15e6b7360e3aca7b7de6478da97ea
Dean Hay <deanhay16@gmail.com>
2022-06-04 22:15:49 +1000

    added client route api

--- controllers/api/clientRoutes.js
+++ controllers/api/clientRoutes.js
@@ -1,3 +1,32 @@
-const router = require('express').Router();
+const { Client } = require("../../models");
+const router = require("express").Router();
 
+router.get("/");
+
+router.post("/", async (req, res) => {
+    try {
+        const addClient = await Client.create({
+            clientData: req.body.clientData,
+            location: req.body.location,
+            budget: req.body.budget,
+            clientId: req.body.clientId
+
+        });
+
+        // Serialize data so the template can read it
+        const Client = addClient.get({ plain: true });
+
+        // Pass serialized data and session flag into template
+        res.render("client", {
+            client,
+            logged_in: req.session.logged_in,
+        });
+
+        console.log("addClient", addClient);
+        res.status(200).json(addClient);
+    } catch (err) {
+        console.error(err);
+        res.status(400).json(err);
+    }
+});
 module.exports = router;
