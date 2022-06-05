3a5da60ceccff1a09eeff3aac5ade2166e9b57a6
Tim Polo <timothyjpolo@gmail.com>
2022-06-05 12:23:39 +1000

    domain api 50% done

--- public/js/projectView.js
+++ public/js/projectView.js
@@ -8,16 +8,18 @@ const cityAddress = document.getElementById('city_address').textContent.replace(
 // Declare Global Variables ---------------------------------
 const googleMapsAPIKey = "AIzaSyBZSbspfBXqcmanZk33s7O_cjZnyS3X2r4";
 const googleGeocodingAPIKey = "AIzaSyA3xEm83IcSrmNBMoTLR5PQHJ1WZ9Jr6kY";
+const domainAPIKey = 'key_fe3c219649342187fcd7449017a171bc';
 let map;
 // Object has .lat, .long , .address and .touristAttractionsSearchURL variables
 const cityGoogleObject = {};
 const defaultCityName = 'Sydney';
+const domainObject = {};
 const defaultCityCoords = { lat: -33.8688, lng: 151.2093 }; // Alice Springs
 let cityCoords = {};
 let searchCity = streetAddress + ' ' + cityAddress;
 
 
-// Tim ------------------------------------------------------------
+// Google API ------------------------------------------------------------
 
 function initMap() {
     // The location of thisCity
@@ -60,15 +62,15 @@ function cityGoogleInfo() {
             cityGoogleObject.address = data.results[0].formatted_address;
             cityCoords.lat = cityGoogleObject.lat;
             cityCoords.lng = cityGoogleObject.long;
-
-            console.log(typeof (cityCoords));
-            console.log(defaultCityCoords);
         });
 }
 
 async function init() {
 
     await cityGoogleInfo();
+    await getDomainPropertyID();
+    await getDomainLocationID();
+    await getDomainInfo();
     window.initMap = initMap;
     // Setup Google maps section
     // Create the script tag, set the appropriate attributes for Initial Google Map. initMap function called.
@@ -78,5 +80,49 @@ async function init() {
     document.head.appendChild(script);
 
 }
+
+// Domain API   ----------------------------------------------------
+// https://api.domain.com.au/v1/properties/RF-8884-AK/priceEstimate 
+// `https://api.domain.com.au/v1/properties/_suggest?terms=1%20Smith%20Street%2C%20Smithfield%2C%20NSW&pageSize=20&channel=residential&api_key=${domainAPIKey}`;
+
+function getDomainPropertyID() {
+    let domainRequest = `https://api.domain.com.au/v1/properties/_suggest?terms=${searchCity}&pageSize=1&channel=residential&api_key=${domainAPIKey}`;
+
+    return fetch(domainRequest)
+        .then((response) => response.json())
+        .then((data) => {
+            domainObject.propertyID = data[0].id;
+            domainObject.addressComponents = data[0].addressComponents;
+        });
+}
+
+function getDomainLocationID() {
+    //https://api.domain.com.au/v1/addressLocators
+    //   ?searchLevel=Address&streetNumber=100
+    //   &streetName=Harris&streetType=Street
+    //   &suburb=Pyrmont&state=NSW&postcode=2009
+    let domainRequest = `https://api.domain.com.au/v1/addressLocators
+?searchLevel=Address&streetNumber=${domainObject.addressComponents.streetNumber}&streetName=${domainObject.addressComponents.streetName}&streetType=${domainObject.addressComponents.streetType}&suburb=${domainObject.addressComponents.suburb}&state=${domainObject.addressComponents.state}&postcode=${domainObject.addressComponents.postCode}&api_key=${domainAPIKey}`;
+
+    return fetch(domainRequest)
+        .then((response) => response.json())
+        .then((data) => {
+            domainObject.ids = data[0].ids;
+            console.log(domainObject.ids);
+        });
+}
+
+// https://api.domain.com.au/v1/locations/profiles/41352
+function getDomainInfo() {
+    let domainRequest = `https://api.domain.com.au/v1/locations/profiles/${domainObject.ids[2].id}&api_key=${domainAPIKey}`;
+
+    return fetch(domainRequest)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        });
+
+}
+// -------------------------------------------------------
 
 init();
