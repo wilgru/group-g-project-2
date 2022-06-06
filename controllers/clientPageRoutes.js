82d89a308f3f47460077fbebd98041084bf3e4c6
ElsieMay <elsiemaylawrie1@gmail.com>
2022-06-06 19:22:13 +1000

    added to readme

--- controllers/clientPageRoutes.js
+++ controllers/clientPageRoutes.js
@@ -90,25 +90,4 @@ router.get("/view/:id", withAuth, async (req, res) => {
 	}
 });
 
-router.delete("/:id", withAuth, async (req, res) => {
-	try {
-		const clientData = await Client.destroy({
-			where: {
-				id: req.params.id,
-				user_id: req.session.user_id,
-			},
-		});
-
-		if (!clientData) {
-			res.status(404).json({ message: "No client found with this id!" });
-			return;
-		}
-
-		res.status(200).json(clientData);
-	} catch (err) {
-		console.error(err);
-		res.status(500).json(err);
-	}
-});
-
 module.exports = router;
